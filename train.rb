8e0cc79e732f0260c325ebb108341556711d6fa8
DragonGhost0971 <generals86sur@gmail.com>
2020-11-03 15:56:53 +0300

    Tree commit

--- train.rb
+++ train.rb
@@ -1,30 +1,36 @@
 class Train
-  def initialize
-  	@nameber = nameber
-  	@type = type
-  	@nameber_of_cars
-  end
+	def initialize(number,type,number_of_cars)
+		@number = number
+		@type = type
+		@number_of_cars = number_of_cars
+	end
 
-  def speed
-  	@speed = 50
-  end
+	def speed
+		@speed = 60
+	end
 
-  def currrent_speed
-  	puts "currrent_speed: #{@speed}"
-  end
+	def currrent_speed
+		puts "currrent_speed: #{@speed}"
+	end
 
-  def stop
-  	@speed = 0
-  end
+	def stop
+		@speed = 0
+	end
 
-  def namber_of_cars
-  	puts "number of cars: #{nameber_of_cars}"
-  end
+	def number_of_cars
+		@number_of_cars
+	end
 
-  def add_car
-  	if @speed == 0
-  		@namber_of_cars +=1
-  	puts "add car"
-  end
-  
-  end
+	def add_car
+		if @speed == 0
+			@number_of_cars+=1
+		end
+	end
+
+	def minus_car
+		if @speed == 0
+			@number_of_cars-=1
+		end
+	end
+	
+end
