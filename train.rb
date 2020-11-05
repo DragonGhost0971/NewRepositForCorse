6bfa667760b2e6dbd6b866308f3576395288254f
DragonGhost0971 <generals86sur@gmail.com>
2020-11-05 19:55:43 +0300

    New commit

--- train.rb
+++ train.rb
@@ -1,36 +1,93 @@
 class Train
-	def initialize(number,type,number_of_cars)
-		@number = number
-		@type = type
-		@number_of_cars = number_of_cars
-	end
 
-	def speed
-		@speed = 60
-	end
+attr_reader :speed, :type, :number_of_cars
 
-	def currrent_speed
-		puts "currrent_speed: #{@speed}"
-	end
+  def initialize(number,type,number_of_cars)
+	@number = number
+	@type = type
+	@number_of_cars = number_of_cars
+	@route
+  end
 
-	def stop
-		@speed = 0
-	end
+  def go
+	  @speed = 60
+  end
 
-	def number_of_cars
-		@number_of_cars
-	end
+  def stop
+	  @speed = 0
+  end
 
-	def add_car
-		if @speed == 0
-			@number_of_cars+=1
-		end
-	end
+  def attach_a_car
+	  if @speed == 0
+		@number_of_cars+=1
+	  end
+  end
 
-	def minus_car
-		if @speed == 0
-			@number_of_cars-=1
-		end
-	end
-	
+  def uncouple_a_car
+	  if @speed == 0
+	 	  @number_of_cars-=1
+	  end
+  end
+
+  def routes(route)
+	@route=route
+	route.start.take_of_the_train(self)
+		
+  end
+
+  def next_one
+	@route.travel.take_of_the_train(self)
+  end	
+end
+
+class Station
+
+  def initialize(name_station)
+  	@name_station = name_station
+  	@trains = []
+  end
+
+  def take_of_the_train(train)
+  	@trains<<train
+  end
+
+  def delete_the_train(train)
+  	@trains.delete(train)
+  end
+
+  def show_trains
+  	@trains.each {|train| puts train}  
+  end
+
+  def type_train(type)
+  	@trains.each do |train|
+  		puts train if type == train.type
+  	end
+  end
+end
+
+
+class Route 
+attr_reader :start
+attr_reader :travel
+
+  def initialize(start,finish)
+  	@start = start
+  	@finish = finish
+  	@travel = [start,finish]
+  end
+
+  def add_station(station)
+  	@travel.delete_at(-1)
+  	@travel<<station
+  	@travel<<@finish
+  end
+
+  def del_station(station)
+  	@travel.delete(station)
+  end
+
+  def list_route
+  	@travel.each {|station| puts station.name_station}
+  end
 end
