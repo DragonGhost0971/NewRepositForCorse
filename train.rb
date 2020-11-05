5f3203266d7f1de33ce38fa666406ae0c743d151
DragonGhost0971 <generals86sur@gmail.com>
2020-11-05 22:04:08 +0300

    last

--- train.rb
+++ train.rb
@@ -1,93 +1,94 @@
 class Train
+  attr_reader :speed, :type, :number_of_cars, :route
 
-attr_reader :speed, :type, :number_of_cars
-
-  def initialize(number,type,number_of_cars)
-	@number = number
-	@type = type
-	@number_of_cars = number_of_cars
-	@route
+  def initialize(number, type, number_of_cars)
+    @number = number
+    @type = type
+    @number_of_cars = number_of_cars
   end
 
   def go
-	  @speed = 60
+    @speed = 60
   end
 
   def stop
-	  @speed = 0
+    @speed = 0
   end
 
   def attach_a_car
-	  if @speed == 0
-		@number_of_cars+=1
-	  end
+    @number_of_cars += 1 if @speed == 0
   end
 
   def uncouple_a_car
-	  if @speed == 0
-	 	  @number_of_cars-=1
-	  end
+    @number_of_cars -= 1 if @speed == 0
   end
 
   def routes(route)
-	@route=route
-	route.start.take_of_the_train(self)
-		
+    @route = route
+    route.start.take_of_the_train(self)
+
   end
 
   def next_one
-	@route.travel.take_of_the_train(self)
-  end	
+    @route.travel[1].take_of_the_train(self)
+    @route.travel[0].delete_the_train(self)
+  end
+
+  def back_one
+  	@route.travel[0].take_of_the_train(self)
+    @route.travel[1].delete_the_train(self)
+  end
+  def view_routs
+  puts @route.travel.each
+
+  end
 end
 
 class Station
-
   def initialize(name_station)
-  	@name_station = name_station
-  	@trains = []
+    @name_station = name_station
+    @trains = []
   end
 
   def take_of_the_train(train)
-  	@trains<<train
+    @trains << train
   end
 
   def delete_the_train(train)
-  	@trains.delete(train)
+    @trains.delete(train)
   end
 
   def show_trains
-  	@trains.each {|train| puts train}  
+    @trains.each { |train| puts train }
   end
 
   def type_train(type)
-  	@trains.each do |train|
-  		puts train if type == train.type
-  	end
+    @trains.each do |train|
+      puts train if type == train.type
+    end
   end
 end
 
+class Route
+  attr_reader :start, :travel
 
-class Route 
-attr_reader :start
-attr_reader :travel
-
-  def initialize(start,finish)
-  	@start = start
-  	@finish = finish
-  	@travel = [start,finish]
+  def initialize(start, finish)
+    @start = start
+    @finish = finish
+    @travel = [start, finish]
   end
 
   def add_station(station)
-  	@travel.delete_at(-1)
-  	@travel<<station
-  	@travel<<@finish
+    @travel.delete_at(-1)
+    @travel << station
+    @travel << @finish
   end
 
   def del_station(station)
-  	@travel.delete(station)
+    @travel.delete(station)
   end
 
   def list_route
-  	@travel.each {|station| puts station.name_station}
+    @travel.each { |station| puts station.name_station }
   end
 end
